191450984bc14b8e0d63b47808e685b9ba2a0bdf
javex <florian.ruechel@gmail.com>
2013-10-31 01:07:25 +0100

    Add test command to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 from __future__ import unicode_literals, absolute_import
-from distutils.core import setup
+from distutils.core import setup, Command
 import os
 import re
+import subprocess
+import sys
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
 
 vfile = open(os.path.join(os.path.dirname(__file__), 'pysess', '__init__.py'))
 VERSION = re.match(r".*__version__ = '(.*?)'", vfile.read(), re.S).group(1)
@@ -11,6 +28,7 @@ rfile = open(os.path.join(os.path.dirname(__file__), 'README.md'))
 readme = rfile.read()
 rfile.close()
 
+
 setup(
     name='PySess',
     version=VERSION,
@@ -18,4 +36,5 @@ setup(
     license='MIT',
     description="A python web session package to make sessions easy.",
     long_description=readme,
+    cmdclass={'test': PyTest},
 )
